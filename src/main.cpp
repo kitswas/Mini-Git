e5d06ffbc171b204067e5d0c67872dcb1b2d5b34
kitswas <90329875+kitswas@users.noreply.github.com>
2024-10-23 19:42:19 +0530

    Partially Implemented cat-file command

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <zstr.hpp> // ZLib C++ wrapper. See https://github.com/mateidavid/zstr
 
 int main(int argc, char *argv[])
 {
@@ -39,6 +40,50 @@ int main(int argc, char *argv[])
 		catch (const std::filesystem::filesystem_error &e)
 		{
 			std::cerr << e.what() << '\n';
+			return EXIT_FAILURE;
+		}
+	}
+	else if (command == "cat-file")
+	{
+		if (argc < 4)
+		{
+			std::cerr << "Invalid arguments, `-p <blob_sha>` required.\n";
+			return EXIT_FAILURE;
+		}
+
+		if (const std::string flag = argv[2]; flag != "-p")
+		{
+			std::cerr << "Invalid flag for cat-file, `-p` expected\n";
+			return EXIT_FAILURE;
+		}
+
+		const std::string value = argv[3];
+		const std::string dir_name = value.substr(0, 2);
+		const std::string blob_sha = value.substr(2);
+
+		std::string path = ".mygit/objects/" + dir_name + "/" + blob_sha;
+
+		try
+		{
+			zstr::ifstream input(path, std::ofstream::binary);
+
+			if (!input.is_open())
+			{
+				std::cerr << "Failed to open object file.\n";
+				return EXIT_FAILURE;
+			}
+
+			std::string object_str{std::istreambuf_iterator<char>(input),
+								   std::istreambuf_iterator<char>()};
+
+			input.close();
+
+			const std::string object_content = object_str.substr(object_str.find('\0') + 1);
+			std::cout << object_content;
+		}
+		catch (...)
+		{
+			std::cerr << "Failed to open object file.\n";
 			return EXIT_FAILURE;
 		}
 	}
